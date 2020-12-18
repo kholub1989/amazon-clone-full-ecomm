661159f0904c4ad5984f8cee43a70cf8b95ec7eb
Krisztian Holub <kholub1989@gmail.com>
2020-12-18 12:38:34 -0500

    loading func from server

--- web/src/screens/HomeScreen.js
+++ web/src/screens/HomeScreen.js
@@ -1,16 +1,38 @@
-import React from "react";
-
-import data from "../data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "../components/Product";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-      <div className="row center">
-        {data.product.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox>{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
