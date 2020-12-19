fd7f8a953805a5bf493f525bd0baa34110b159b6
Krisztian Holub <kholub1989@gmail.com>
2020-12-19 16:17:45 -0500

    add cartItems bage with base styles

--- web/src/App.js
+++ web/src/App.js
@@ -4,8 +4,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <Router>
       <div className="grid-container">
@@ -16,7 +19,12 @@ function App() {
             </Link>
           </div>
           <div>
-            <Link to="/cart">Cart</Link>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
             <Link to="/signin">Sign In</Link>
           </div>
         </header>
