feb9baa604a04016831a3b1dcb27d24da3717833
Krisztian Holub <kholub1989@gmail.com>
2020-12-20 13:17:11 -0500

    create loyout for cart add logic to display items

--- web/src/screens/CartScreen.js
+++ web/src/screens/CartScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../actions/cartActions";
+import { Link } from "react-router-dom";
+import MessageBox from "../components/MessageBox";
 
 export default function CartScreen(props) {
   const productId = props.match.params.id;
@@ -8,6 +10,8 @@ export default function CartScreen(props) {
     ? Number(props.location.search.split("=")[1])
     : 1;
 
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   const dispatch = useDispatch();
   useEffect(() => {
     if (productId) {
@@ -15,11 +19,43 @@ export default function CartScreen(props) {
     }
   }, [dispatch, productId, qty]);
   return (
-    <div>
-      <h1>Cart Screen</h1>
-      <p>
-        ADD TO CART : ProductID: {productId} Qty: {qty}
-      </p>
+    <div className="row top">
+      <div class="col-2">
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <MessageBox>
+            Cart is empty. <Link to="/">Go Shopping</Link>
+          </MessageBox>
+        ) : (
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.product}>
+                <div className="row">
+                  <img src={item.image} alt={item.name} className="small" />
+                </div>
+                <div class="min-30">
+                  <Link to={`/product/${item.product}`}>{item.product}</Link>
+                </div>
+                <div>
+                  <select
+                    value={item.qty}
+                    onChange={(e) =>
+                      dispatch(addToCart(item.product), Number(e.target.value))
+                    }
+                  >
+                    {[...Array(item.countInStock).keys()].map((x) => (
+                      <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div>$ {item.price}</div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
