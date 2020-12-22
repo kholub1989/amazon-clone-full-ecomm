542fb0a43c39b8854ca7874483ac6cd6d527f48c
Krisztian Holub <kholub1989@gmail.com>
2020-12-22 11:06:10 -0500

    return token and data

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,6 +3,7 @@ import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import data from "../data.js";
 import User from "../models/userModel.js";
+import { generateToken } from "../utils.js";
 
 const userRouter = express.Router();
 
