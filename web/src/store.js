8c8451bdf382358842bd3b5fbb4c1abc2bb6f5d7
Krisztian Holub <kholub1989@gmail.com>
2020-12-23 12:49:00 -0500

    add reducer to store.js

--- web/src/store.js
+++ web/src/store.js
@@ -5,6 +5,7 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
+import { userSigninReducer } from "./reducers/userReducer";
 
 const initialState = {
   cart: {
@@ -17,6 +18,7 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
