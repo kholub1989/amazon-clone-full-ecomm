b89cddeb9a9553d89287b25edffe344b0229ed4e
Krisztian Holub <kholub1989@gmail.com>
2020-12-26 13:13:17 -0500

    create register action and reducer

--- web/src/reducers/userReducer.js
+++ web/src/reducers/userReducer.js
@@ -1,4 +1,7 @@
 import {
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_FAIL,
   USER_SIGNIN_SUCCESS,
@@ -18,6 +21,22 @@ export const userSigninReducer = (state = {}, action) => {
       };
     case USER_SIGNOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
