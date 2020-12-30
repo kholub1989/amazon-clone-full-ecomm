51c2b280e5b1d38af6c069e59568d21d3d772a95
Krisztian Holub <kholub1989@gmail.com>
2020-12-30 13:15:17 -0500

    crearte payment method screen

---
+++ web/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { CheckoutSteps } from "../components/CheckoutSteps";
+
+export default function PaymentMethodScreen() {
+  const [paymentMethod, setpaymentMethod] = useState("PayPal");
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    props.history.push("/placeorder");
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>Payment</div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="paypal"
+              value="PayPal"
+              name="paymentMethod"
+              required
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></input>
+            <label htmlFor="paypal">PayPal</label>
+          </div>
+        </div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="stripe"
+              value="Stripe"
+              name="paymentMethod"
+              required
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></input>
+            <label htmlFor="stripe">Stripe</label>
+          </div>
+        </div>
+        <div>
+          <button className="primary" type="submit">
+            Continue
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
