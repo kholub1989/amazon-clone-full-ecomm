e0de1f026f6e105ebdd15603fac6bb32d241b045
Krisztian Holub <kholub1989@gmail.com>
2021-01-07 11:14:03 -0500

    create order action

--- web/src/screens/PlaceOrderScreen.js
+++ web/src/screens/PlaceOrderScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { createOrder } from "../actions/orderActions";
 import CheckoutSteps from "../components/CheckoutSteps";
 
 export default function PlaceOrderScreen(props) {
