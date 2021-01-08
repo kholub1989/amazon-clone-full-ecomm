a4b917769c9a66e65a2ea90ebc9803055ff2f395
Krisztian Holub <kholub1989@gmail.com>
2021-01-08 14:42:45 -0500

    create deatailsOrder action and add constants

--- web/src/actions/orderActions.js
+++ web/src/actions/orderActions.js
@@ -3,6 +3,8 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_FAIL,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_FAIL,
 } from "../constants/orderConstants";
 import { CART_EMPTY } from "../constants/cartConstants";
 
@@ -32,5 +34,18 @@ export const createOrder = (order) => async (dispatch, getState) => {
 };
 
 export const detailsOrder = (orderId) => (dispatch, getState) => {
-  
-}
+  dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
+  const {userSignin: {userInfo}} = getState()
+  try {
+    const {data} = await Axios.get(`/api/orders/${orderId}`, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({type: ORDER_DETAILS_SUCCESS, payload: data})
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: ORDER_DETAILS_FAIL, payload: message });
+  }
+};
