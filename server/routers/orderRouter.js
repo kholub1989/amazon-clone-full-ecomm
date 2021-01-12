9be28ebb0b30997e9e772d202da20eec8e749b77
Krisztian Holub <kholub1989@gmail.com>
2021-01-12 10:46:30 -0500

    fix bugs with tabel loyaut

--- server/routers/orderRouter.js
+++ server/routers/orderRouter.js
@@ -8,7 +8,7 @@ const orderRouter = express.Router();
 orderRouter.get(
   "/mine",
   isAuth,
-  expressAsyncHandler(async (res, req) => {
+  expressAsyncHandler(async (req, res) => {
     const orders = await Order.find({ user: req.user._id });
     res.send(orders);
   })
--- web/src/screens/OrderHistoryScreen.js
+++ web/src/screens/OrderHistoryScreen.js
@@ -20,7 +20,7 @@ export default function OrderHistoryScreen(props) {
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <table className="table">
-          <table>
+          <thead>
             <tr>
               <th>ID</th>
               <th>DATE</th>
@@ -29,13 +29,13 @@ export default function OrderHistoryScreen(props) {
               <th>DELIVERED</th>
               <th>ACTIONS</th>
             </tr>
-          </table>
+          </thead>
           <tbody>
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
-                <td>{order.createdAt}</td>
-                <td>{order.totalPrice}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>{Number(order.totalPrice).toFixed(2)}</td>
                 <td>{order.isPaid ? order.paidAt.substring(0, 10) : "No"}</td>
                 <td>
                   {order.isDelivered
